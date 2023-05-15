18dbf8fb360da65936128c0538bfbd743ce78a5e
Julien Clavel <julien.clavel@etu.unistra.fr>
2023-05-15 20:19:30 +0200

    WIDOW FIXED

--- src/packageModel/chessPiece/Widow.java
+++ src/packageModel/chessPiece/Widow.java
@@ -7,13 +7,10 @@ import java.util.Queue;
 
 public class Widow extends NonEmpty{
     private MoveList availableMoves;
-    private final Piece queen,knight;
     public Widow(boolean white, Coord pos) {
         super(white, pos);
         availableMoves = new MoveList();
         Object[] init = {white,pos};
-        queen = Factory.newPiece(PcId.QUEEN,init);
-        knight = Factory.newPiece(PcId.QUEEN,init);
         if(white) this.imgPath = "chessImage/white_widow.png";
         else this.imgPath = "chessImage/black_widow.png";
     }
@@ -35,21 +32,107 @@ public class Widow extends NonEmpty{
 
     @Override
     public ArrayList<Coord> allCapturePos(Board board) {
-        queen.setPos(getPos());
-        knight.setPos(getPos());
         ArrayList<Coord> res = new ArrayList<>();
-        res.addAll(queen.allCapturePos(board));
-        res.addAll(knight.allCapturePos(board));
+        Coord[] direction = new Coord[8];
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(i != 1 || j != 1)
+                    direction[(i*3+j<4)?(3*i+j):(3*i+j-1)] = new Coord(i-1,j-1);
+            }
+        }
+        Coord start = getPos();
+        Coord tmp;
+        for( Coord dir: direction) {
+            tmp = start;
+            do {
+                tmp = Coord.addCoord(tmp,dir);
+                Coord.addToArray(res,tmp);
+            }
+            while(board.canMove(tmp));
+        }
+        for(int i = -1; i < 2 ; i += 2){
+            for(int j = -1 ; j < 2 ; j += 2){
+                Coord.addToArray(res,getPos().addXY(2*j,i));
+                Coord.addToArray(res,getPos().addXY(j,2*i));
+            }
+        }
         return res;
+
     }
 
     @Override
     public void setValidMoves(Board board) {
         availableMoves.clear();
-        queen.setValidMoves(board);
-        knight.setValidMoves(board);
-        availableMoves = (MoveList) queen.getValidMoves().clone();
-        availableMoves.addAll(knight.getValidMoves());
+        availableMoves = genAllQ(board);
+        availableMoves.addAll(genAllK(board));
+        
+    }
+
+    private MoveList genAllQ(Board board){
+        MoveList res = new MoveList();
+        Coord[] direction = new Coord[8];
+        Object[] builder = new Object[4];
+        Coord start = getPos();
+        Coord tmp;
+        Move toTest;
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(i != 1 || j != 1)
+                    direction[(i*3+j<4)?(3*i+j):(3*i+j-1)] = new Coord(i-1,j-1);
+            }
+        }
+        builder[0] = this;
+        for( Coord dir: direction) {
+
+            tmp = Coord.addCoord(start,dir);
+
+            while(board.canMove(tmp)){
+                builder[1] = tmp;
+                toTest = Factory.newMove(MoveId.BASIC,builder);
+                if(!board.isCheck(isWhite(),toTest))
+                    res.add(toTest);
+                tmp = Coord.addCoord(tmp,dir);
+            }
+
+            if(board.canCapture(this,tmp)){
+
+                builder[1] = tmp;
+                builder[2] =  board.getPiece(tmp);
+                toTest = Factory.newMove(MoveId.CAPTURE,builder);
+                if(!board.isCheck(isWhite(),toTest))
+                    res.add(toTest);
+            }
+        }
+        return res;
+    }
+    private MoveList genAllK(Board board) {
+        MoveList res = new MoveList();
+        Object[] builder = new Object[4];
+        builder[0] = this;
+        Coord start = getPos(), z[] = new Coord[2];
+        Move toTest;
+
+        for(int i = -1; i < 2 ; i+= 2){
+            for(int j = -1 ; j < 2 ; j+= 2){
+                z[0] = start.addXY(2*j,i);
+                z[1] = start.addXY(j, 2*i);
+                for(Coord tmp: z){
+                    if(board.canMove(tmp)){
+                        builder[1] = tmp;
+                        toTest = Factory.newMove(MoveId.BASIC,builder);
+                        if(!board.isCheck(isWhite(),toTest))
+                            res.add(toTest);
+                    }else if(board.canCapture(this,tmp)){
+                        builder[1] = tmp;
+                        builder[2] =  board.getPiece(tmp);
+                        toTest = Factory.newMove(MoveId.CAPTURE,builder);
+                        if(!board.isCheck(isWhite(),toTest))
+                            res.add(toTest);
+                    }
+                }
+            }
+        }
+        return res;
     }
     
     @Override
